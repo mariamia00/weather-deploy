a533cce0739de9b6a12c4edb6814f66b3ea95e7a
mariamia00 <138917445+mariamia00@users.noreply.github.com>
2023-11-02 23:33:48 +0200

    try

---
+++ backend/built/app.js
@@ -0,0 +1,34 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
+var user_router_1 = __importDefault(require("./routers/user.router"));
+var database_config_1 = require("./database.config");
+var post_router_1 = __importDefault(require("./routers/post.router"));
+var path_1 = __importDefault(require("path")); // Import 'path' module
+var mongoose_1 = __importDefault(require("mongoose"));
+var user_model_1 = require("./models/user.model");
+var contact_router_1 = __importDefault(require("./routers/contact.router"));
+var comments_router_1 = __importDefault(require("./routers/comments.router"));
+(0, database_config_1.dbConnect)();
+var app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)({
+    credentials: true,
+    origin: ["http://localhost:4200"],
+}));
+var UserModel = mongoose_1.default.model("User", user_model_1.UserSchema);
+app.use("/uploads", express_1.default.static(path_1.default.join(__dirname, "uploads")));
+app.use("/api/users", user_router_1.default);
+app.use("/api", post_router_1.default);
+app.use("/api", contact_router_1.default);
+app.use("/api/comments", comments_router_1.default);
+var port = process.env.PORT || 5000;
+app.listen(port, function () {
+    console.log("website served on http://localhost:" + port);
+});
---
+++ backend/built/models/contact.model.js
@@ -0,0 +1,28 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var mongoose_1 = __importDefault(require("mongoose"));
+// Create a Mongoose schema for Contact
+var contactSchema = new mongoose_1.default.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    message: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+// Create and export the Contact model
+var ContactModel = mongoose_1.default.model("Form", contactSchema);
+exports.default = ContactModel;
