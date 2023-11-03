891baee4e9e2ee4615a8c0b3d5f4a7c317a0f54d
mariamia00 <138917445+mariamia00@users.noreply.github.com>
2023-11-03 12:52:19 +0200

    localhost fix 2

--- frontend/src/app/constants/urls.ts
+++ frontend/src/app/constants/urls.ts
@@ -1,6 +1,10 @@
-import { environment } from "src/environments/environment";
+import { environment } from 'src/environments/environment';
 
-const BASE_URL = environment.production? '' : 'http://localhost:5000';
+const BASE_URL = environment.production ? '' : 'http://localhost:5000';
+
+if (environment.production) {
+  console.log('this is the enviroment production: ' + environment.production);
+}
 
 export const USER_LOGIN_URL = BASE_URL + '/api/users/login';
 export const USER_REGISTER_URL = BASE_URL + '/api/users/register';
