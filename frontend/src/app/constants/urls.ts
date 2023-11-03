3843bfcdf0bedbefbc944ef1ebbdae055a20cab1
mariamia00 <138917445+mariamia00@users.noreply.github.com>
2023-11-03 17:47:34 +0200

    fixing local

--- frontend/src/app/constants/urls.ts
+++ frontend/src/app/constants/urls.ts
@@ -1,6 +1,8 @@
 import { environment } from 'src/environments/environment';
 
-const BASE_URL = environment.production ? '' : 'http://localhost:5000';
+const BASE_URL = environment.production
+  ? 'https://crazyweather.onrender.com'
+  : 'http://localhost:5000';
 
 export const USER_LOGIN_URL = BASE_URL + '/api/users/login';
 export const USER_REGISTER_URL = BASE_URL + '/api/users/register';
