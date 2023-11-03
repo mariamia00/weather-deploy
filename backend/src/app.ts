ed161e2e91a74c9d00d5342c09018dbf6c93ce1e
mariamia00 <138917445+mariamia00@users.noreply.github.com>
2023-11-03 02:01:23 +0200

    sasssdf

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -24,12 +24,14 @@ app.use(
 
 const UserModel = mongoose.model("User", UserSchema);
 
-app.use(express.static(path.join(__dirname, "frontend", "dist")));
+app.use(express.static(path.join(__dirname, "frontend", "dist", "weather")));
 
 // Handle other routes
 app.get("*", (req: any, res: any) => {
   // Serve the Angular "index.html" for all other routes
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(
+    path.join(__dirname, "frontend", "dist", "weather", "index.html")
+  );
 });
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
