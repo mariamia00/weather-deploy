47909654188e3d2c9d76b6272c4a5ef7fc998aab
mariamia00 <138917445+mariamia00@users.noreply.github.com>
2023-11-03 13:00:07 +0200

    localhost fix 3

--- backend/built/app.js
+++ backend/built/app.js
@@ -1,9 +1,7 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
@@ -20,34 +18,23 @@ var comments_router_1 = __importDefault(require("./routers/comments.router"));
 (0, database_config_1.dbConnect)();
 var app = (0, express_1.default)();
 app.use(express_1.default.json());
-app.use(
-  (0, cors_1.default)({
+app.use((0, cors_1.default)({
     credentials: true,
-    origin: ["http://localhost:4200"],
-  })
-);
+    origin: ["https://crazyweather.onrender.com"],
+}));
 var UserModel = mongoose_1.default.model("User", user_model_1.UserSchema);
 // Serve static files from the Angular dist directory
-app.use(
-  express_1.default.static(
-    path_1.default.join(__dirname, "../../frontend/dist/weather/")
-  )
-);
+app.use(express_1.default.static(path_1.default.join(__dirname, "../../frontend/dist/weather/")));
 // Define a catch-all route that serves your Angular app's index.html
 app.get("*", function (req, res) {
-  res.sendFile(
-    path_1.default.join(__dirname, "../../frontend/dist/weather/index.html")
-  );
+    var filePath = res.sendFile(path_1.default.join(__dirname, "../../frontend/dist/weather/index.html"));
 });
-app.use(
-  "/uploads",
-  express_1.default.static(path_1.default.join(__dirname, "uploads"))
-);
+app.use("/uploads", express_1.default.static(path_1.default.join(__dirname, "uploads")));
 app.use("/api/users", user_router_1.default);
 app.use("/api", post_router_1.default);
 app.use("/api", contact_router_1.default);
 app.use("/api/comments", comments_router_1.default);
 var port = process.env.PORT || 5000;
 app.listen(port, function () {
-  console.log("website served on http://localhost:" + port);
+    console.log("website served on http://localhost:" + port);
 });
